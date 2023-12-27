5125f4be763ebc770c044d11a3abdc7490258bc3
Melody G <melodyflash@gmail.com>
2023-12-27 15:06:28 -0800

    init commit

---
+++ src/employees.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import {createRoot} from 'react-dom/client'
+import EmployeeList from './EmployeeList.jsx'
+
+// const root = createRoot(document.getElementById('content'))
+// root.render(
+ReactDOM.render(
+  <React.StrictMode>
+    <EmployeeList />
+  </React.StrictMode>,
+document.getElementById('content'))
